2b24a3774ece603d4e8ccae1ebc8b3af16b01d5b
elukashova <ele.lukashova@gmail.com>
2022-10-02 21:45:39 +0200

    feat: add 320px media query main page

--- online-zoo/pages/main/script.js
+++
@@ -1,7 +0,0 @@
-// const hamburger = document.querySelector(".hamburger");
-// const mainMenu = document.querySelector(".links");
-
-// hamburger.addEventListener("click",() => {
-//     mainMenu.classList.toggle("active");
-//     hamburger.classList.toggle("active");
-// })

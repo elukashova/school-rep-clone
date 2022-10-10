3b575934c12d957f34a5b8795614373a2730a893
elukashova <ele.lukashova@gmail.com>
2022-10-11 00:44:15 +0200

    refactor: fix animation issues

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -11,186 +11,200 @@ hamburger.onclick = function() {
 
 //CAROUSEL PETS
 const SOURCE_DATA = [
-    {
-      name: "giant-panda",
-      title: "GIANT PANDAS",
-      description: "Native to Southwest China",
-      type: "grass"
-    },
-    {
-      name: "sloth",
-      title: "TWO-TOED SLOTH",
-      description: "Mesoamerica, South America",
-      type: "grass"
-    },
-    {
-      name: "eagles",
-      title: "EAGLES",
-      description: "Native to South America",
-      type: "meat"
-    },
-    {
-      name: "cheet",
-      title: "CHEETAHS",
-      description: "Native to Africa",
-      type: "meat"
-    },
-    {
-      name: "gorilla",
-      title: "GORILLAS",
-      description: "Native to Congo",
-      type: "grass"
-    },
-    {
-      name: "alligator",
-      title: "ALLIGATORS",
-      description: "Native to Southeastern U. S.",
-      type: "meat"
-    },
-    {
-      name: "koala",
-      title: "KOALA",
-      description: "Native to Australia",
-      type: "grass"
-    },
-    {
-      name: "elephant",
-      title: "ELEPHANT",
-      description: "Native to Africa",
-      type: "grass"
-    },
-    {
-      name: "zubr",
-      title: "ZUBR",
-      description: "Native to Belarus",
-      type: "grass"
-    },
-    {
-      name: "giraffe",
-      title: "GIRAFFE",
-      description: "Native to Africa",
-      type: "grass"
-    },
-    {
-      name: "pantera",
-      title: "PANTERA",
-      description: "Native to Asia",
-      type: "meat"
-    },
-    {
-      name: "lion",
-      title: "LION",
-      description: "Native to Africa",
-      type: "meat"
-    },
-    {
-      name: "seal",
-      title: "SEAL",
-      description: "Native to the Arctic",
-      type: "meat"
-    },
-    {
-      name: "alpaca",
-      title: "ALPACA",
-      description: "Native to Peru",
-      type: "grass"
-    },
-    {
-      name: "brown-bear",
-      title: "BROWN BEAR",
-      description: "Native to Eurasia",
-      type: "meat"
-    },
-    {
-      name: "lemur",
-      title: "LEMUR",
-      description: "Native to Madagaskar",
-      type: "grass"
-    },
-    {
-      name: "rhinos",
-      title: "RHINOS",
-      description: "Native to Africa",
-      type: "grass"
-    }
-  ];
-  
-  const animalIcon = {
-    meat: "meet-fish_icon",
-    grass: "banana-bamboo_icon"
-  };
-  
-  const MAX_CARDS_SET = 6;
-  const UNIQUE_CARDS = [];
-  
-  const uniqueIndex = () => {
-    return Math.floor(Math.random() * SOURCE_DATA.length);
-  };
-  
+  {
+    name: 'giant-panda',
+    title: 'GIANT PANDAS',
+    description: 'Native to Southwest China',
+    type: 'grass',
+  },
+  {
+    name: 'sloth',
+    title: 'TWO-TOED SLOTH',
+    description: 'Mesoamerica, South America',
+    type: 'grass',
+  },
+  {
+    name: 'eagles',
+    title: 'EAGLES',
+    description: 'Native to South America',
+    type: 'meat',
+  },
+  {
+    name: 'cheet',
+    title: 'CHEETAHS',
+    description: 'Native to Africa',
+    type: 'meat',
+  },
+  {
+    name: 'gorilla',
+    title: 'GORILLAS',
+    description: 'Native to Congo',
+    type: 'grass',
+  },
+  {
+    name: 'alligator',
+    title: 'ALLIGATORS',
+    description: 'Native to Southeastern U. S.',
+    type: 'meat',
+  },
+  {
+    name: 'koala',
+    title: 'KOALA',
+    description: 'Native to Australia',
+    type: 'grass',
+  },
+  {
+    name: 'elephant',
+    title: 'ELEPHANT',
+    description: 'Native to Africa',
+    type: 'grass',
+  },
+  {
+    name: 'zubr',
+    title: 'ZUBR',
+    description: 'Native to Belarus',
+    type: 'grass',
+  },
+  {
+    name: 'giraffe',
+    title: 'GIRAFFE',
+    description: 'Native to Africa',
+    type: 'grass',
+  },
+  {
+    name: 'pantera',
+    title: 'PANTERA',
+    description: 'Native to Asia',
+    type: 'meat',
+  },
+  {
+    name: 'lion',
+    title: 'LION',
+    description: 'Native to Africa',
+    type: 'meat',
+  },
+  {
+    name: 'seal',
+    title: 'SEAL',
+    description: 'Native to the Arctic',
+    type: 'meat',
+  },
+  {
+    name: 'alpaca',
+    title: 'ALPACA',
+    description: 'Native to Peru',
+    type: 'grass',
+  },
+  {
+    name: 'brown-bear',
+    title: 'BROWN BEAR',
+    description: 'Native to Eurasia',
+    type: 'meat',
+  },
+  {
+    name: 'lemur',
+    title: 'LEMUR',
+    description: 'Native to Madagaskar',
+    type: 'grass',
+  },
+  {
+    name: 'rhinos',
+    title: 'RHINOS',
+    description: 'Native to Africa',
+    type: 'grass',
+  },
+];
+
+const ANIMAL_ICON = {
+  meat: 'meet-fish_icon',
+  grass: 'banana-bamboo_icon',
+};
+
+const MAX_CARDS_SET = 6;
+const UNIQUE_CARDS = [];
+
+const getRandomIndex = () => {
+  return Math.floor(Math.random() * SOURCE_DATA.length);
+};
+
+const fillUniqCards = () => {
+  UNIQUE_CARDS.length = 0;
+
   while (UNIQUE_CARDS.length < MAX_CARDS_SET) {
-    const INDEX = uniqueIndex();
+    const INDEX = getRandomIndex();
     if (!UNIQUE_CARDS.includes(INDEX)) {
       UNIQUE_CARDS.push(INDEX);
     }
   }
-  
-  const createCard = (animalData) => {
-    return `<div class="foto-card-container">
-  <img class="img-card" src="../../assets/images/${animalData.name}.jpg">
-  <div class="box-bottom">
-  <div class="cards-text">
-  <p class="card-title">${animalData.title}</p>
-  <p class="card-descr">${animalData.description}</p>
-  </div>
-  <div class="icon-card" id="icon-card2">
-  <img src="../../assets/icons/${animalIcon[animalData.type]}.svg">
-  </div>
-  </div>
-  </div>`;
-  };
-  
-  const rightBtn = document.querySelector("#next");
-  const leftBtn = document.querySelector("#prev");
-  const root = document.querySelector(".scroll-animals");
-  const current = document.querySelector(".current-items");
-  
-  const createCards = (elem) => {
-    const FINAL = [];
-    for (let i = 0; i < UNIQUE_CARDS.length; i++) {
-      let index = UNIQUE_CARDS[i];
-      const CARD = createCard(SOURCE_DATA[index]);
-      FINAL.push(CARD);
-    }
-    return elem.insertAdjacentHTML("afterbegin", FINAL.join(""));
-  };
-  
-  function generateNewItems(side) {
-    const wrapper = document.createElement("div");
-    wrapper.setAttribute("class", side === "right" ? "js-next items" : "js-prev items");
-    createCards(wrapper);
-    side === "right" ? root.append(wrapper) : root.insertBefore(wrapper, current);
+};
+
+const createCard = (animalData) => {
+  return `
+    <div class="foto-card-container">
+      <img class="img-card" src="../../assets/images/${animalData.name}.jpg">
+      <div class="box-bottom">
+        <div class="cards-text">
+          <p class="card-title">${animalData.title}</p>
+          <p class="card-descr">${animalData.description}</p>
+        </div>
+        <div class="icon-card" id="icon-card2">
+          <img src="../../assets/icons/${ANIMAL_ICON[animalData.type]}.svg">
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+const rightBtn = document.querySelector('#next');
+const leftBtn = document.querySelector('#prev');
+const root = document.querySelector('.scroll-animals');
+
+const createCards = (elem) => {
+  fillUniqCards();
+  const result = [];
+  for (let i = 0; i < UNIQUE_CARDS.length; i++) {
+    let index = UNIQUE_CARDS[i];
+    const CARD = createCard(SOURCE_DATA[index]);
+    result.push(CARD);
   }
+  return elem.insertAdjacentHTML('afterbegin', result.join(''));
+};
 
-  const jsPrev = document.querySelector(".js-prev");
-  
-  rightBtn.addEventListener("click", () => {
-    generateNewItems("right");
-    const jsNext = document.querySelector(".js-next");
-    jsNext.classList.add("transition-right");
-    jsNext.classList.remove("transition-right");
-    root.removeChild(document.querySelector(".current-items"));
-    const nextElem = document.querySelector(".js-next");
-    nextElem.classList.remove("js-next");
-    nextElem.classList.add("current-items");
-    setTimeout(() => {
-    }, 1100);
-  });
+function generateNewItems(side) {
+  const wrapper = document.createElement('div');
+  wrapper.className = side === 'right' ? 'js-next items' : 'js-prev items';
+  createCards(wrapper);
+  side === 'right' ? root.append(wrapper) : root.prepend(wrapper);
+}
 
+rightBtn.addEventListener('click', () => {
+  generateNewItems('right');
+  const currentEl = document.querySelector('.current-items');
+  const nextElClassList = document.querySelector('.js-next').classList;
+  nextElClassList.add('transition-right');
 
+  setTimeout(() => {
+    nextElClassList.remove('transition-right');
+    root.removeChild(currentEl);
+    nextElClassList.add('current-items');
+    nextElClassList.remove('js-next');
+  }, 1000);
+});
 
-  leftBtn.addEventListener("click", () => {
-    generateNewItems("left");
-  });
+leftBtn.addEventListener('click', () => {
+    generateNewItems('left');
+    const currentEl = document.querySelector('.current-items');
+    const nextElClassList = document.querySelector('.js-prev').classList;
+    nextElClassList.add('transition-left');
+
+  setTimeout(() => {
+    nextElClassList.remove('transition-left');
+    root.removeChild(currentEl);
+    nextElClassList.add('current-items');
+    nextElClassList.remove('js-prev');
+  }, 1000);
+
+});
 
 
 // const moveRight = () => {

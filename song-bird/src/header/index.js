3a3242f05a09456be7ed2a9dc3a4204ebf03dad3
elukashova <ele.lukashova@gmail.com>
2022-11-07 22:50:39 +0100

    feat: add header layout

---
+++ song-bird/src/header/index.js
@@ -0,0 +1,7 @@
+import htmlToElement from '../utils/html-to-element';
+import Header from './index.html';
+import './index.scss';
+
+const header = htmlToElement(Header);
+
+export default header;
---
+++ song-bird/src/index.js
@@ -0,0 +1,8 @@
+import './index.scss';
+import Header from './header/index';
+
+const body = document.getElementById('body');
+
+body.append(Header);
+
+console.log(Header);

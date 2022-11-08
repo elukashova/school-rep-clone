9ff5d4473cac29c87ef285e3c78d4625a0a06962
elukashova <ele.lukashova@gmail.com>
2022-11-08 22:53:00 +0100

    refactor: change project structure

---
+++ song-bird/src/pages/game-page/section-01/index.js
@@ -0,0 +1,7 @@
+import htmlToElement from '../../../utils/html-to-element';
+import Section01 from './index.html';
+import './index.scss';
+
+const upperSection = htmlToElement(Section01);
+
+export default upperSection;
---
+++ song-bird/src/pages/game-page/section-02/index.js
@@ -0,0 +1,7 @@
+import htmlToElement from '../../../utils/html-to-element';
+import Section02 from './index.html';
+import './index.scss';
+
+const gameSection = htmlToElement(Section02);
+
+export default gameSection;

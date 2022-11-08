4bd4ad0bf85d953aed7d7dbc97fe7831cd121c1a
elukashova <ele.lukashova@gmail.com>
2022-11-09 00:25:35 +0100

    feat: add footer

---
+++ song-bird/src/components/footer/index.js
@@ -0,0 +1,7 @@
+import htmlToElement from '../../utils/html-to-element';
+import Footer from './index.html';
+import './index.scss';
+
+const footer = htmlToElement(Footer);
+
+export default footer;
--- song-bird/src/index.js
+++ song-bird/src/index.js
@@ -2,9 +2,11 @@ import './index.scss';
 import Header from './components/header/index';
 import upperSection from './pages/game-page/section-01/index';
 import gameSection from './pages/game-page/section-02/index';
+import Footer from './components/footer/index';
 
 const body = document.getElementById('body');
 
 body.append(Header);
 body.append(upperSection);
 body.append(gameSection);
+body.append(Footer);

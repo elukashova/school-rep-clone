fdf28b55ceafcf21261f19975ea392a5211665d3
elukashova <ele.lukashova@gmail.com>
2022-11-10 20:39:03 +0100

    feat: implement custom audio players

--- song-bird/src/index.js
+++ song-bird/src/index.js
@@ -1,12 +1,36 @@
 import './index.scss';
 import Header from './components/header/index';
-import upperSection from './pages/game-page/section-01/index';
-import gameSection from './pages/game-page/section-02/index';
+import UpperSection from './pages/game-page/section-01/index';
+import GameSection from './pages/game-page/section-02/index';
 import Footer from './components/footer/index';
+import PlayAudio from './pages/game-page/section-02/audio-player';
+
 
 const body = document.getElementById('body');
 
 body.append(Header);
-body.append(upperSection);
-body.append(gameSection);
+body.append(UpperSection);
+body.append(GameSection);
 body.append(Footer);
+
+
+
+//***REGULATE AUDIO - QUESTION***
+const playIcon = document.getElementById("btn-play");
+const audioRange = document.getElementById("qstn-audio-range");
+const audioPlayed = document.getElementById("audio-time");
+const audioTotal = document.getElementById("audio-total");
+const volumeRange = document.getElementById("qstn-volume-range");
+const audio = document.getElementById("qstn-audio");
+const volumeIcon = document.getElementById("qstn-btn-volume");
+PlayAudio(playIcon, audioRange, audioPlayed, audioTotal, volumeRange, audio, volumeIcon)
+
+//***REGULATE AUDIO DESCRIPTION***
+const playIconB = document.getElementById("bird-btn-play");
+const audioRangeB = document.getElementById("bird-audio-range");
+const audioPlayedB = document.getElementById("bird-audio-time");
+const audioTotalB = document.getElementById("bird-audio-total");
+const volumeRangeB = document.getElementById("bird-volume-range");
+const audioB = document.getElementById("bird-audio");
+const volumeIconB = document.getElementById("bird-btn-volume");
+PlayAudio(playIconB, audioRangeB, audioPlayedB, audioTotalB, volumeRangeB, audioB, volumeIconB)
---
+++ song-bird/src/pages/game-page/section-02/audio-player.js
@@ -0,0 +1,116 @@
+export default function playAudio(playBtn, aRange, aPlayed, aTotal, vRange, a, volBtn) {
+//play a
+playBtn.addEventListener("click", () => {
+  if(a.paused) {
+    a.play();
+  } else {
+    a.pause();
+  }
+})
+
+//change play button
+a.addEventListener("play", () => {
+  playBtn.src = "../../../assets/icons/pause-btn.svg";
+})
+a.addEventListener("pause", () => {
+  playBtn.src = "../../../assets/icons/play-btn.svg";
+})
+
+//show progress
+const showProgress = (input) => {
+  if (input === aRange) {
+    aRange.style.setProperty("--progress-width", aRange.value / aRange.max * 100 + '%');
+  } else {
+    vRange.style.setProperty("--progress-width", vRange.value / vRange.max * 100 + '%');
+  }
+}
+
+//calculate time
+const calcTime = (secs) => {
+  const m = Math.floor(secs / 60);
+  const s = Math.floor(secs % 60);
+  const finalS = s < 10 ? `0${s}` : `${s}`;
+  return `${m}:${finalS}`;
+}
+
+a.addEventListener ("loadeddata", () => {
+  showProgress(aRange);
+})
+
+//track time
+a.addEventListener ("loadedmetadata", () => {
+  showProgress(aRange);
+  aPlayed.innerHTML = calcTime(0);
+  aTotal.innerHTML = calcTime(a.duration);
+  aRange.max = Math.floor(a.duration);
+  let changedTime = false;
+
+  aRange.addEventListener("input", () => {
+    showProgress(aRange);
+    a.currentTime = aRange.value;
+    changedTime = false;
+  })
+
+  aRange.addEventListener("change", () => {
+    showProgress(aRange);
+    a.currentTime = aRange.value;
+    if(!a.paused) {
+      a.play();
+    }
+    changedTime = false;
+  })
+
+  a.addEventListener("timeupdate", () => {
+    if(!changedTime) {
+      aRange.value = Math.floor(a.currentTime);
+      aRange.setAttribute("value",  aRange.value);
+      showProgress(aRange);
+      aPlayed.innerHTML = calcTime(aRange.value);
+    }
+  })
+})
+//volume
+vRange.addEventListener("change", () => {
+  volume();
+  vRange.setAttribute("value", vRange.value);
+})
+
+vRange.addEventListener("input", () => {
+  volume();
+  vRange.setAttribute("value",  vRange.value);
+})
+
+const aIcon = (input) => {
+  if(input.value > 0.1 && input.value <= 0.4) {
+    volBtn.src = "../../../assets/icons/volume-low.svg"
+  } else if(input.value > 0.4 && input.value <= 0.9) {
+    volBtn.src = "../../../assets/icons/volume-mid.svg"
+  }else if(input.value == 0) {
+    volBtn.src = "../../../assets/icons/volume-off.svg"
+  } else {
+    volBtn.src = "../../../assets/icons/volume-up.svg"
+  }
+}
+
+let soundOn = true;
+volBtn.addEventListener("click", () => {
+  let temp;
+  if (vRange.value > 0) {
+    temp = vRange.value;
+  }
+  soundOn = !soundOn;
+  if(soundOn === false) {
+    vRange.value = 0;
+    volume();
+  } else {
+    vRange.value = temp;
+    volume();
+  }
+})
+
+const volume = () => {
+  a.volume = vRange.value;
+  aIcon(vRange);
+  showProgress(vRange);
+}
+}

e36fb595b10260af21fb1db674bab20a36c9eb52
elukashova <ele.lukashova@gmail.com>
2022-11-11 14:49:59 +0100

    feat: add functions for current question and answers

--- song-bird/src/index.js
+++ song-bird/src/index.js
@@ -4,6 +4,7 @@ import UpperSection from './pages/game-page/section-01/index';
 import GameSection from './pages/game-page/section-02/index';
 import Footer from './components/footer/index';
 import PlayAudio from './pages/game-page/section-02/audio-player';
+import BirdsData from './pages/game-page/section-02/birds-data';
 
 
 const body = document.getElementById('body');
@@ -23,7 +24,7 @@ const audioTotal = document.getElementById("audio-total");
 const volumeRange = document.getElementById("qstn-volume-range");
 const audio = document.getElementById("qstn-audio");
 const volumeIcon = document.getElementById("qstn-btn-volume");
-PlayAudio(playIcon, audioRange, audioPlayed, audioTotal, volumeRange, audio, volumeIcon)
+PlayAudio(playIcon, audioRange, audioPlayed, audioTotal, volumeRange, audio, volumeIcon);
 
 //***REGULATE AUDIO DESCRIPTION***
 const playIconB = document.getElementById("bird-btn-play");
@@ -33,4 +34,89 @@ const audioTotalB = document.getElementById("bird-audio-total");
 const volumeRangeB = document.getElementById("bird-volume-range");
 const audioB = document.getElementById("bird-audio");
 const volumeIconB = document.getElementById("bird-btn-volume");
-PlayAudio(playIconB, audioRangeB, audioPlayedB, audioTotalB, volumeRangeB, audioB, volumeIconB)
+PlayAudio(playIconB, audioRangeB, audioPlayedB, audioTotalB, volumeRangeB, audioB, volumeIconB);
+
+//***PLAYING CONSTS***
+const checkbox = document.getElementsByClassName("answers__item_input");
+
+//bird card
+const currBirdImg = document.getElementById("bird-img");
+const currBirdName = document.getElementById("bird-name");
+const currBirdLatin = document.getElementById("bird-name-latin");
+const currBirdAudio = document.getElementById("bird-audio");
+const currBirdText = document.getElementById("bird-text");
+
+
+
+const birdsData = BirdsData;
+
+class Quiz {
+  constructor () {
+    this.birdData = {};
+    this.id = "";
+    this.categories = {
+      first: 0,
+      second: 1,
+      third: 2,
+      fourth: 3,
+      fifth: 4,
+      sixth: 5
+    }
+    this.category = this.categories.first;
+    this.currBird = "";
+    this.answersList = [];
+    //this.answerTry = "";
+    this.isRight = false;
+    this.count = 0;
+    this.clicks = 0;
+    this.score = 0;
+    this.finish = false;
+  }
+
+  go() {
+    this.createQstn();
+    this.listAnswers();
+    this.checkAnswer();
+  }
+
+  createQstn() {
+    let cat = this.category;
+    let randomNum = Math.floor(Math.random() * 6);
+    let currBird = birdsData[cat][randomNum];
+    let currQstnData = {
+      name: currBird.name,
+      species: currBird.species,
+      descr: currBird.description,
+      image: currBird.image,
+      audio: currBird.audio
+    }
+
+    this.birdData = currQstnData;
+    this.currBird = currQstnData.name;
+
+    let qstnAudio = currQstnData.audio;
+    audio.src = qstnAudio;
+  }
+
+  listAnswers() {
+    let cat = this.category;
+    let answers = birdsData[cat].map((value) => {
+      return value.name;
+    })
+    this.answersList = answers;
+    for (let i = 0; i < answers.length; i++) {
+      let placeholder =  document.getElementById(`bird-${i}`);
+      let checkbox = document.getElementById(`${i}`);
+      checkbox.value = answers[i];
+      placeholder.innerText = answers[i];
+      if(answers[i] === this.birdData.name) {
+        checkbox.classList.add("answer__correct");
+      }
+    }
+  }
+  
+}
+
+let songBird = new Quiz;
+songBird.go()
+

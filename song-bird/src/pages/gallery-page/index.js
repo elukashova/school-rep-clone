461f2ba9a312e5988acb19c454c66386ee1d4cfe
elukashova <ele.lukashova@gmail.com>
2022-11-17 16:21:14 +0100

    feat: add gallery layout

---
+++ song-bird/src/pages/gallery-page/index.js
@@ -0,0 +1,7 @@
+import htmlToElement from '../../utils/html-to-element';
+import GalleryPage from './index.html';
+import './index.scss';
+
+const galleryPage = htmlToElement(GalleryPage);
+
+export default galleryPage;

e55f767b0554a83da5524fac026c19d094a696bb
elukashova <ele.lukashova@gmail.com>
2022-11-21 18:58:00 +0100

    fix: resolve bug with current category highlighting

--- song-bird/src/index.js
+++ song-bird/src/index.js
@@ -61,8 +61,9 @@ linkStart.addEventListener ("click", () => {
 })
 
 linkGallery.addEventListener ("click", () => {
+  galleryCat = 0;
   replacePages(GalleryPage, StartPage, GamePage, ResultsPage);
-  createGallery(0, lang);
+  createGallery(galleryCat, lang);
   activateLink(linkGallery);
 })
 
@@ -473,6 +474,9 @@ const starterPack = () => {
 
 //highlight the current category
 const changeCategory = (cat) => {
+  const categories = document.querySelectorAll(".questions__category");
+  updateCategory(categories);
+
   if (cat === 0) {
     let previous = document.getElementById(`cat-5`);
     let current = document.getElementById(`cat-${cat}`);
@@ -485,6 +489,19 @@ const changeCategory = (cat) => {
     current.classList.add("questions__category_current");
   }
   chooseRandom(currCat);
+}
+
+//remove category highlight
+const updateCategory = (categories) => {
+  categories.forEach(cat => {
+    if (cat.classList.contains("questions__category_current")) {
+      cat.classList.remove("questions__category_current");
+    } else if (cat.classList.contains("gallery__category_current")) {
+      let current = document.getElementById("gal-cat-0");
+      cat.classList.remove("gallery__category_current");
+      current.classList.add("gallery__category_current");
+    }
+  })
 }
 
 //ending the game
@@ -544,6 +561,12 @@ const createGallery = (cat, lang) => {
   const cardTitle = document.querySelectorAll(".card__title");
   const cardLatin = document.querySelectorAll(".card__latin-name");
   const cardAudio = document.querySelectorAll(".card__audio_player");
+  const categories = document.querySelectorAll(".gallery__category");
+
+  if (galleryCat === 0) {
+    updateCategory(categories);
+  }
+  
   let source;
   if (lang === 'ru') {
     source = birdsData[cat];

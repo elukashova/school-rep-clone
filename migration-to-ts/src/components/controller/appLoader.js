6bf0cc4f2536e2fd576b2b3abe80e44c5286e9d1
elukashova <ele.lukashova@gmail.com>
2022-12-07 21:47:33 +0100

    feat: add apiKey

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '59573672e8be4a728354bedcc494a09b', // получите свой ключ https://newsapi.org/
         });
     }
 }

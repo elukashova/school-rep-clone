3a3c404ef4a429ec23cb95db51bc1c0241f79ebd
elukashova <ele.lukashova@gmail.com>
2022-12-08 19:40:42 +0100

    feat: add types for loader

---
+++ migration-to-ts/src/components/controller/loader.ts
@@ -0,0 +1,48 @@
+import { IOptions, Errors } from './loader.types';
+
+class Loader {
+    constructor(baseLink: string, options: IOptions) {
+        this.baseLink = baseLink;
+        this.options = options;
+    }
+
+    getResp(
+        { endpoint, options = {} },
+        callback = () => {
+            console.error('No callback for GET response');
+        }
+    ) {
+        this.load('GET', endpoint, callback, options);
+    }
+
+    errorHandler(res: Response): Response {
+        if (!res.ok) {
+            if (res.status === Errors.Unauthorized || res.status === Errors.NotFound)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+
+        return res;
+    }
+
+    makeUrl(options: IOptions, endpoint: string): string {
+        const urlOptions = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    load(method, endpoint, callback, options = {}): void {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res: Response) => res.json())
+            .then((data: JSON) => callback(data))
+            .catch((err: Error) => console.error(err));
+    }
+}
+
+export default Loader;

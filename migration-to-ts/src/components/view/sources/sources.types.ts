9cd11f85c3a63b4a90f7788f222bd3efdccdeeea
elukashova <ele.lukashova@gmail.com>
2022-12-17 20:26:09 +0100

    refactor: apply prettier

--- migration-to-ts/src/components/view/sources/sources.types.ts
+++ migration-to-ts/src/components/view/sources/sources.types.ts
@@ -1,15 +1,14 @@
-/* eslint-disable prettier/prettier */
 export interface ISources {
-  draw: (data: SourcesData[]) => void;
-  undraw: (data: SourcesData[]) => void;
+    draw: (data: SourcesData[]) => void;
+    undraw: (data: SourcesData[]) => void;
 }
 
 export interface SourcesData {
-  category: string;
-  country: string;
-  description: string;
-  id: string;
-  language: string;
-  name: string;
-  url: string;
+    category: string;
+    country: string;
+    description: string;
+    id: string;
+    language: string;
+    name: string;
+    url: string;
 }
